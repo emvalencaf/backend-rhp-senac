0c9f6aa3287350356bef06ea2ff4cdfd8c86b487
Afonso <afonsoestevao04@gmail.com>
2024-12-03 14:50:20 -0300

    feat: Adição de Cors

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-# main.py
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from datetime import date
 from sqlalchemy.orm import Session, joinedload
 from typing import List
 from cron import lifespan_scheduler
 from local_stage import save_local_stage
-from models.models import Unidade, Paciente, Leito, Transferencia, Alta, Atendimento, Profissional # Importa os models
+from models.models import Unidade, Paciente, Leito, Transferencia, Alta, Atendimento, Profissional  # Importa os models
 from database import SessionLocal, engine, Base  # Importa a sessão e a engine do database.py
 from pydantic import BaseModel
 from sqlalchemy.exc import OperationalError, DatabaseError
@@ -21,13 +21,28 @@ from controllers.transferenciaController import router as transferencia_router
 from controllers.unidadeController import router as unidade_router
 
 # Cria o aplicativo FastAPI
-# app = FastAPI(lifespan=lifespan_scheduler) ativar o cron job
 app = FastAPI()
 
-#====== Rota Publica apenas para testar no Postman se esta tudo funcionando======
+#====== Configuração do CORS (permite requisições de outras origens) ======
+origem_permitida = [
+    "http://localhost",            # Permite localhost
+    "http://localhost:3000",       # Permite frontend React (ou outra porta, se necessário)
+    "http://127.0.0.1",           # Permite localhost com IP
+    "http://127.0.0.1:3000",      # Permite frontend React no 127.0.0.1
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origem_permitida,  # Lista de origens permitidas
+    allow_credentials=True,
+    allow_methods=["*"],             # Permite todos os métodos (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],             # Permite todos os headers
+)
+
+# Rota pública apenas para testar no Postman se está tudo funcionando
 @app.get("/")
 def root():
-    return {"message": "API esta funcionando!"}
+    return {"message": "API está funcionando!"}
 
 # Cria as tabelas no banco de dados
 Base.metadata.create_all(bind=engine)
